dc5f4e71e5ed2c5111b7858866cb98318f6b55d7
igor.sharov <i.sharov@mail.ru>
2019-10-08 14:21:03 +0300

    Lesson_2

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Описание фрукта
+name = "apple"
+color = "green"
+amount = 2
+is_tasty = True
+weight_kg = 0.25
+is_rotten = False
+
+print(type(name))
+print(type(color))
+print(type(amount))
+print(type(is_tasty))
+print(type(weight_kg))
+print(type(is_rotten))
